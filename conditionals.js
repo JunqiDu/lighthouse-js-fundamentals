a0d022317e759171aa4c09d5816efda7a3c3050f
Junqi Du <dujunqi@hotmail.com>
2022-12-05 21:12:36 +0000

    Use conditionals to tell people about the weather

---
+++ conditionals.js
@@ -0,0 +1,17 @@
+//Web Immersive Prep Course
+//Conditional Execution
+//Junqi Du
+//Dec 05 2022
+
+const raining = true;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
